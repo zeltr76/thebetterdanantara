51686d62c218c68de8c943301521ca9aead07cbd
Richard <108578204+zeltr76@users.noreply.github.com>
2025-03-17 19:40:43 +0800

    fix(components/Hero.vue): change Tablet layout

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,23 +1,25 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="mx-auto mt-16 max-w-screen-xl px-2 md:mt-24 md:px-0">
-    <div
-      class="flex h-[250px] flex-col items-center justify-center gap-1 rounded-md bg-cover bg-center shadow-xl md:h-[500px] md:gap-2"
-      style="
-        background-image: url(&quot;https://www.danantaraindonesia.com/images/v3/hero-children-indo.png&quot;);
-      "
-    >
-      <p
-        class="font-eb-garamond text-[30px] font-semibold leading-none text-background drop-shadow-xl md:text-[96px]"
+  <div class="mx-2 mt-16 xl:mt-24">
+    <div class="mx-auto max-w-screen-xl">
+      <div
+        class="flex h-[250px] flex-col items-center justify-center gap-1 rounded-md bg-cover bg-center shadow-xl md:h-[350px] xl:h-[500px] xl:gap-2"
+        style="
+          background-image: url(&quot;https://www.danantaraindonesia.com/images/v3/hero-children-indo.png&quot;);
+        "
       >
-        Daya Anagata Nusantara
-      </p>
-      <p
-        class="font-eb-garamond text-xl leading-none text-background drop-shadow-xl md:text-5xl"
-      >
-        "Demi Kemakmuran Indonesia"
-      </p>
+        <p
+          class="font-eb-garamond text-[30px] font-semibold leading-none text-background drop-shadow-xl md:text-[60px] xl:text-[96px]"
+        >
+          Daya Anagata Nusantara
+        </p>
+        <p
+          class="font-eb-garamond text-xl leading-none text-background drop-shadow-xl md:text-[40px] xl:text-5xl"
+        >
+          "Demi Kemakmuran Indonesia"
+        </p>
+      </div>
     </div>
   </div>
 </template>
