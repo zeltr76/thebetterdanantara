82231499836bb65bcb9291f615b31f069c483f5c
Richard <108578204+zeltr76@users.noreply.github.com>
2025-03-18 15:37:23 +0800

    fix(components/Hero.vue):
    - remove rounded-md classes
    - change the Hero height in mobile
    - adjust the text aligment and the font size
    - use AtomicSectionHeading

--- components/Hero.vue
+++ components/Hero.vue
@@ -16,17 +16,17 @@ onMounted(() => {
 <template>
   <div class="mx-auto max-w-screen-xl">
     <div
-      class="relative flex h-[400px] flex-col items-center justify-center gap-1 rounded-md bg-cover bg-center shadow-xl md:h-[350px] xl:h-[500px] xl:gap-2"
+      class="relative flex h-[600px] flex-col items-center justify-center gap-1 bg-cover bg-center shadow-xl md:h-[350px] xl:h-[500px] xl:gap-2"
       :style="{ 'background-image': `url(${randomImage})` }"
     >
-      <div class="absolute inset-0 rounded-md bg-black/40"></div>
+      <div class="absolute inset-0 bg-black/40"></div>
       <p
-        class="font-eb-garamond text-[30px] font-semibold leading-none text-background drop-shadow-xl md:text-[60px] xl:text-[96px]"
+        class="text-center font-eb-garamond text-[60px] font-semibold leading-none text-background drop-shadow-xl xl:text-start xl:text-[96px]"
       >
         Daya Anagata Nusantara
       </p>
       <p
-        class="font-eb-garamond text-xl leading-none text-background drop-shadow-xl md:text-[40px] xl:text-5xl"
+        class="text-center font-eb-garamond text-xl leading-none text-background drop-shadow-xl md:text-[40px] xl:text-start xl:text-5xl"
       >
         "Demi Kemakmuran Indonesia"
       </p>
