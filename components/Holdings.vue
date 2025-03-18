c2691c5c233cc5dc3817c2d483ac0834622d7d3e
Richard <108578204+zeltr76@users.noreply.github.com>
2025-03-18 15:42:53 +0800

    fix(components/Holdings.vue): make the images `lazy` load (hopefully)

--- components/Holdings.vue
+++ components/Holdings.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="mx-auto max-w-[1000px] space-y-4">
-    <h1 class="text-center text-2xl font-bold uppercase">Holdings</h1>
+    <AtomicSectionHeading>Holdings</AtomicSectionHeading>
 
     <div
       class="grid grid-cols-2 items-center justify-items-center gap-4 md:grid-cols-3 xl:grid-cols-4"
@@ -11,36 +11,43 @@
         src="/public/images/pertamina-logo.webp"
         alt="Pertamina logo"
         class="w-32 xl:w-48"
+        loading="lazy"
       />
       <img
         src="/public/images/mind-id-logo.webp"
         alt="Mining Industry Indonesia logo"
         class="w-32 xl:w-48"
+        loading="lazy"
       />
       <img
         src="/public/images/bank-mandiri-logo.webp"
         alt="Bank Mandiri logo"
         class="w-32 xl:w-48"
+        loading="lazy"
       />
       <img
         src="/public/images/bank-bri-logo.webp"
         alt="Bank Rakyat Indonesia logo"
         class="w-32 xl:w-48"
+        loading="lazy"
       />
       <img
         src="/public/images/pln-logo.webp"
         alt="Pembangkit Listrik Negara logo"
         class="w-16 xl:w-24"
+        loading="lazy"
       />
       <img
         src="/public/images/telkom-logo.webp"
         alt="Telkom logo"
         class="w-32 xl:w-48"
+        loading="lazy"
       />
       <img
         src="/public/images/bank-bni-logo.webp"
         alt="Bank Negara Indonesia logo"
         class="w-28 xl:w-48"
+        loading="lazy"
       />
     </div>
   </div>
