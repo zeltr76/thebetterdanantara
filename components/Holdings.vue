48f1d723779ef05904f71ce47c4735b355db692c
Richard <108578204+zeltr76@users.noreply.github.com>
2025-03-18 22:09:40 +0800

    fix(components/Holdings.vue): change images

--- components/Holdings.vue
+++ components/Holdings.vue
@@ -1,52 +1,54 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const images = ref([
+  {
+    id: 1,
+    src: "/images/pertamina-logo.webp",
+    alt: "Pertamina logo",
+  },
+  {
+    id: 2,
+    src: "/images/mind-id-logo.webp",
+    alt: "Mining Industry Indonesia logo",
+  },
+  {
+    id: 3,
+    src: "/images/bmri-logo.webp",
+    alt: "Bank Mandiri logo",
+  },
+  {
+    id: 4,
+    src: "/images/bbri-logo.webp",
+    alt: "Bank Rakyat Indonesia logo",
+  },
+  {
+    id: 5,
+    src: "/images/pln-logo.webp",
+    alt: "Pembangkit Listrik Negara logo",
+  },
+  {
+    id: 6,
+    src: "/images/telkom-logo.webp",
+    alt: "Telkom logo",
+  },
+  {
+    id: 7,
+    src: "/images/bbni-logo.webp",
+    alt: "Bank Negara Indonesia logo",
+  },
+])
+</script>
 
 <template>
   <div class="mx-auto max-w-[1000px] space-y-4">
     <AtomicSectionHeading>Holdings</AtomicSectionHeading>
 
     <div
-      class="grid grid-cols-2 items-center justify-items-center gap-4 md:grid-cols-3 xl:grid-cols-4"
+      class="grid grid-cols-2 justify-items-center gap-4 md:grid-cols-3 xl:grid-cols-4"
     >
       <img
-        src="/public/images/pertamina-logo.webp"
-        alt="Pertamina logo"
-        class="w-32 xl:w-48"
-        loading="lazy"
-      />
-      <img
-        src="/public/images/mind-id-logo.webp"
-        alt="Mining Industry Indonesia logo"
-        class="w-32 xl:w-48"
-        loading="lazy"
-      />
-      <img
-        src="/public/images/bank-mandiri-logo.webp"
-        alt="Bank Mandiri logo"
-        class="w-32 xl:w-48"
-        loading="lazy"
-      />
-      <img
-        src="/public/images/bank-bri-logo.webp"
-        alt="Bank Rakyat Indonesia logo"
-        class="w-32 xl:w-48"
-        loading="lazy"
-      />
-      <img
-        src="/public/images/pln-logo.webp"
-        alt="Pembangkit Listrik Negara logo"
-        class="w-16 xl:w-24"
-        loading="lazy"
-      />
-      <img
-        src="/public/images/telkom-logo.webp"
-        alt="Telkom logo"
-        class="w-32 xl:w-48"
-        loading="lazy"
-      />
-      <img
-        src="/public/images/bank-bni-logo.webp"
-        alt="Bank Negara Indonesia logo"
-        class="w-28 xl:w-48"
+        v-for="image in images"
+        :src="image.src"
+        :alt="image.alt"
         loading="lazy"
       />
     </div>
