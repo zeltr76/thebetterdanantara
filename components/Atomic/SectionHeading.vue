a0132b713d898e7520767482e575623ed0754aef
Richard <108578204+zeltr76@users.noreply.github.com>
2025-03-18 22:10:34 +0800

    fix(components/Atomic/SectionHeading.vue): set letter spacing

--- components/Atomic/SectionHeading.vue
+++ components/Atomic/SectionHeading.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1 class="text-center text-lg font-bold uppercase md:text-2xl xl:text-3xl">
+  <h1
+    class="text-center text-lg font-bold uppercase tracking-wider md:text-2xl xl:text-3xl"
+  >
     <slot />
   </h1>
 </template>
