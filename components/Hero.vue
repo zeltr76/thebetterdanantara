d91b65ff566ccf1ecc56eb5ea398f0f79935e342
Richard <108578204+zeltr76@users.noreply.github.com>
2025-03-19 10:11:47 +0800

    fix(components/Hero.vue): fix rendering issue and create a new way to display image

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,25 +1,30 @@
 <script setup lang="ts">
-// I think there's something wrong with this rendering, I think it renders twice in SSR and CSR
 const images = ref([
   "/images/hero-children-indo.webp",
   "/images/building-flag-indo.webp",
 ])
 
-const randomImage = ref(images.value[0])
+const randomImage = ref(null)
 
-onMounted(() => {
+if (!randomImage.value) {
   randomImage.value =
     images.value[Math.floor(Math.random() * images.value.length)]
-})
+}
 </script>
 
 <template>
   <div class="mx-auto max-w-screen-xl">
     <div
-      class="relative flex h-[600px] flex-col items-center justify-center gap-1 bg-cover bg-center shadow-xl md:h-[350px] xl:h-[500px] xl:gap-2"
-      :style="{ 'background-image': `url(${randomImage})` }"
+      class="relative flex h-[600px] flex-col items-center justify-center gap-1 shadow-xl md:h-[350px] xl:h-[500px] xl:gap-2"
     >
+      <img
+        class="absolute inset-0 h-[600px] w-full object-cover md:h-[350px] xl:h-[500px]"
+        :src="randomImage"
+        alt=""
+      />
+
       <div class="absolute inset-0 bg-black/40"></div>
+
       <p
         class="text-center font-eb-garamond text-[60px] font-semibold leading-none text-background drop-shadow-xl xl:text-start xl:text-[96px]"
       >
