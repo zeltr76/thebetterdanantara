fcd2b01f84ed4b6ec188b07a6355b5db27552061
Richard <108578204+zeltr76@users.noreply.github.com>
2025-03-20 23:47:19 +0800

    feat: create new 404 error page

---
+++ pages/[...slug].vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex min-h-screen flex-col items-center justify-center space-y-4">
+    <h1
+      class="text-[60px] font-bold leading-none md:text-[80px] xl:text-[100px]"
+    >
+      404
+    </h1>
+    <p class="max-w-[300px] break-words xl:text-xl">
+      <span class="font-semibold">{{ $route.path }}</span> tidak ditemukan.
+    </p>
+    <NuxtLink
+      to="/"
+      class="rounded-md bg-accent px-2 py-1 uppercase text-background hover:text-text xl:text-lg"
+    >
+      Kembali
+    </NuxtLink>
+  </div>
+</template>
