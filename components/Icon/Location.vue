85c0ea152deecf9b5b63ff243bedd3986881efcb
Richard <108578204+zeltr76@users.noreply.github.com>
2025-03-28 12:01:55 +0800

    fix: change the Icon on Footer

---
+++ components/Icon/Location.vue
@@ -0,0 +1,27 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <circle
+      cx="12"
+      cy="12"
+      r="4"
+      fill="currentColor"
+    />
+    <g
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+    >
+      <path
+        d="M12 4c4.42 0 8 3.58 8 8c0 4.42 -3.58 8 -8 8c-4.42 0 -8 -3.58 -8 -8c0 -4.42 3.58 -8 8 -8Z"
+      />
+      <path d="M12 4v-2M20 12h2M12 20v2M4 12h-2" />
+    </g>
+  </svg>
+</template>
