7a8ad696ed298e99573c19d2c603a0fc212ff934
Richard <108578204+zeltr76@users.noreply.github.com>
2025-03-28 18:22:13 +0800

    feat(components/Hero.vue): change font size

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 const images = ref(["/images/children-indo.webp", "/images/flag-indo.webp"])
 
-const randomImage = ref(null)
+const randomImage = ref()
 
-const imageAlt = ref(null)
+const imageAlt = ref()
 
 const showImage = ref(false)
 
@@ -18,10 +18,12 @@ onMounted(async () => {
       .pop()
       ?.split(".")[0]
       .replace(/-/g, " ")
+      // @ts-ignore
       .replace(/(\w)(\w*)/g, function (g0, g1, g2) {
         return g1.toUpperCase() + g2
       })
 
+  // Wait for html to load and then show image
   await new Promise((resolve) => setTimeout(resolve, 1000))
   showImage.value = true
 })
@@ -33,32 +35,31 @@ onMounted(async () => {
       class="relative flex h-[600px] flex-col items-center justify-center gap-1 shadow-xl md:h-[350px] xl:h-[500px] xl:gap-2"
     >
       <!-- Show img when resolved -->
-      <!-- Otherwise show placeholder -->
-      <!-- Don't say anything about the "alt" -->
       <img
         v-show="showImage"
         class="absolute inset-0 h-[600px] w-full object-cover md:h-[350px] xl:h-[500px]"
         :src="randomImage"
         :alt="imageAlt"
       />
+      <!-- Otherwise show placeholder -->
       <div
         v-show="!showImage"
         class="absolute inset-0 animate-pulse bg-gray-400"
       ></div>
 
-      <!-- Layer -->
+      <!-- Layer below text -->
       <div
         v-show="showImage"
         class="absolute inset-0 bg-black/40"
       ></div>
 
       <p
-        class="text-center font-eb-garamond text-[60px] font-semibold leading-none text-background drop-shadow-xl xl:text-start xl:text-[96px]"
+        class="text-center font-eb-garamond text-7xl font-semibold leading-none text-background drop-shadow-xl md:text-6xl xl:text-start xl:text-8xl"
       >
         Daya Anagata Nusantara
       </p>
       <p
-        class="text-center font-eb-garamond text-[24px] text-xl leading-none text-background drop-shadow-xl md:text-[40px] xl:text-start xl:text-5xl"
+        class="text-center font-eb-garamond text-3xl leading-none text-background drop-shadow-xl md:text-4xl xl:text-start xl:text-5xl"
       >
         "Demi Kemakmuran Indonesia"
       </p>
